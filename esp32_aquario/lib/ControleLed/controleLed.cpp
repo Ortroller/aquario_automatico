2d39e1aee2ddd2b05c2027ddc8903043aa56ed0c
João Pedro Oliveira Santiago <joaopedro1santiago@gmail.com>
2022-06-29 14:23:18 -0300

    Arduino config sensor de temperatura

--- esp32_aquario/lib/ControleLed/controleLed.cpp
+++ esp32_aquario/lib/ControleLed/controleLed.cpp
@@ -28,18 +28,15 @@ bool ControleLED::checkLed(){
     Serial.print(':');
     Serial.print(now.minute(), DEC);
     Serial.println("\n");
-
-    /* -------------------------------------------------------------------------------------------------
-        se HH:mm atual for maior ou igual a hr de permanecer a luz on E 
-        se HH:mm atual for menor ou igual a hora que delimita luz off
-        ENTÃO acende a luz;
-        Caso contrário desliga a luz, ou seja, HH:mm diferente do intervalo de luz on.
-    ------------------------------------------------------------------------------------------------- */
     
-    if((now.hour() >= hrTurnOn && now.minute() >= minTurnOn) || (now.hour() <= hrTurnOff && now.minute() < minTurnOff)) {
+    if(now.hour() == hrTurnOn && now.minute() == minTurnOn) {
         digitalWrite(releLed,HIGH);
         Serial.println("Luz ON");
         return true;
+    } else if (now.hour() == hrTurnOff && now.minute() == minTurnOff) {
+        digitalWrite(releLed,LOW);
+        Serial.println("Luz OFF");
+        return false;
     } else {
         digitalWrite(releLed,LOW);
         Serial.println("Luz OFF");
